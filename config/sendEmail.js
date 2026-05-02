1308f48b57b05908e066963581d9596581097c62
sharuna <sharunamk3@gmail.com>
2026-05-02 19:47:05 +0530

    xoriva

--- config/sendEmail.js
+++ config/sendEmail.js
@@ -2,11 +2,14 @@ const nodemailer = require('nodemailer');
 
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: process.env.EMAIL_USER,
+        clientId: process.env.GMAIL_CLIENT_ID,
+        clientSecret: process.env.GMAIL_CLIENT_SECRET,
+        refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+    }
 });
 
 exports.sendEmail = async (email, otp) => {
